a99a794c44c8dc1df6fe1a7c40405853ae48222c
anthony6444 <anthonydc1972@outlook.com>
2022-02-01 18:10:15 -0600

    formatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,39 +33,38 @@ public final class Constants {
 
         public static final int kPigeonId = 15;
 
-        // vvv From https://github.com/wpilibsuite/allwpilib/blob/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/mecanumcontrollercommand/Constants.java
+        // vvv From
+        // https://github.com/wpilibsuite/allwpilib/blob/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/mecanumcontrollercommand/Constants.java
 
         // Encoder IDs
-        public static final int[] kFrontLeftEncoderIds = new int[] {0, 1};
-        public static final int[] kBackLeftEncoderIds = new int[] {2, 3};
-        public static final int[] kFrontRightEncoderIds = new int[] {4, 5};
-        public static final int[] kBackRightEncoderIds = new int[] {6, 7};
-    
+        public static final int[] kFrontLeftEncoderIds = new int[] { 0, 1 };
+        public static final int[] kBackLeftEncoderIds = new int[] { 2, 3 };
+        public static final int[] kFrontRightEncoderIds = new int[] { 4, 5 };
+        public static final int[] kBackRightEncoderIds = new int[] { 6, 7 };
+
         // Encoder direction
         public static final boolean kFrontLeftEncoderReversed = false;
         public static final boolean kBackLeftEncoderReversed = true;
         public static final boolean kFrontRightEncoderReversed = false;
         public static final boolean kBackRightEncoderReversed = true;
-        
-        // Distance between centers of right and left wheels on robot    
+
+        // Distance between centers of right and left wheels on robot
         public static final double kTrackWidth = 0.56; // 22in
         // Distance between centers of front and back wheels on robot
         public static final double kWheelBase = 0.5; // 20in
-    
-        public static final MecanumDriveKinematics kDriveKinematics =
-            new MecanumDriveKinematics(
+
+        public static final MecanumDriveKinematics kDriveKinematics = new MecanumDriveKinematics(
                 new Translation2d(kWheelBase / 2, kTrackWidth / 2),
                 new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
                 new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
-                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));   // TODO: "The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This class should not be used for any other purpose."
-        
-        public static final double kGearRatio = 10.71; //10.71:1
-        public static final int kEncoderCPR = 42; //counts per revolution
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+        public static final double kGearRatio = 10.71; // 10.71:1
+        public static final int kEncoderCPR = 42; // counts per revolution
         public static final double kWheelDiameterMeters = 0.1524; // 6in
-        public static final double kEncoderDistancePerPulse =
-            (kWheelDiameterMeters * Math.PI) / ((double) kEncoderCPR * kGearRatio); 
-        public static final double kMotorRotationsPerMeter = 
-            (kWheelDiameterMeters * Math.PI) / kGearRatio;
+        public static final double kEncoderDistancePerPulse = (kWheelDiameterMeters * Math.PI)
+                / ((double) kEncoderCPR * kGearRatio);
+        public static final double kMotorRotationsPerMeter = (kWheelDiameterMeters * Math.PI) / kGearRatio;
 
     }
 
@@ -75,7 +74,7 @@ public final class Constants {
     public static final class JoystickConstants {
 
         public static final int kMainJoystickPort = 0;
-        public static final int kAuxJoystickPort  = 4;
+        public static final int kAuxJoystickPort = 4;
 
         public static final double kXDeadZone = 0.1; // Left-Right
         public static final double kYDeadZone = 0.1; // Front-Back
