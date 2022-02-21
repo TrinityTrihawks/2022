5f006667798d036e3314084a042510c6882f9b20
anthony6444 <anthonydc1972@outlook.com>
2022-02-21 10:14:24 -0600

    ?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -60,7 +60,7 @@ public class RobotContainer {
 			() -> mainJoystick.getThrottle());
 
 
-	private final NetworkTable subtable;
+	private final NetworkTable subtable; //?
 
 	/**
 	 * The container for the robot. Contains subsystems, OI devices, and commands.
