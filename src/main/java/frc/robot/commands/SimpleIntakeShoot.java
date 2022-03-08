f75f23ccf84fff4838794d99a084959da157edba
xavier2910 <x.distelzweig@gmail.com>
2022-03-08 16:33:44 -0600

    format

--- src/main/java/frc/robot/commands/SimpleIntakeShoot.java
+++ src/main/java/frc/robot/commands/SimpleIntakeShoot.java
@@ -10,42 +10,43 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShootyBits;
 
 public class SimpleIntakeShoot extends CommandBase {
-  private ShootyBits shooter;
-  private BooleanSupplier shouldRun;
+	private ShootyBits shooter;
+	private BooleanSupplier shouldRun;
 
-  public SimpleIntakeShoot(ShootyBits bits, BooleanSupplier shouldRun) {
+	public SimpleIntakeShoot(ShootyBits bits, BooleanSupplier shouldRun) {
 
-    shooter = bits;
-    this.shouldRun = shouldRun;
+		shooter = bits;
+		this.shouldRun = shouldRun;
 
-    addRequirements(shooter);
-  }
+		addRequirements(shooter);
+	}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    //System.out.println("==============simple intake shoot init");
-  }
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		// System.out.println("==============simple intake shoot init");
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (shouldRun.getAsBoolean()) {
-      shooter.setIntakeVoltage(-0.25);
-      shooter.setShooterVoltage(1);
-      System.out.println("running intake shooter");
-    }
-    shooter.setMiddleVoltage(1);
-    System.out.println("simple intake shoot exec");
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		if (shouldRun.getAsBoolean()) {
+			shooter.setIntakeVoltage(-0.25);
+			shooter.setShooterVoltage(1);
+			System.out.println("running intake shooter");
+		}
+		shooter.setMiddleVoltage(1);
+		System.out.println("simple intake shoot exec");
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
