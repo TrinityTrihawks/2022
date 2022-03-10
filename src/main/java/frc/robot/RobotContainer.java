b8b16679614a0ef3e8c893fde68b6ecb63c0c4f6
xavier2910 <x.distelzweig@gmail.com>
2022-03-10 17:10:13 -0600

    format

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -99,8 +99,8 @@ public class RobotContainer {
                                                                   );
     
     private ParallelCommandGroup runAllWithSlowShooter = new ParallelCommandGroup( runIntake
-                                                                  , runShooterSlow
-                                                                  );
+                                                                                 , runShooterSlow
+                                                                                 );
     
     private final NetworkTable subtable;
 
